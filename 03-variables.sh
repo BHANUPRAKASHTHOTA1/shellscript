dcac4c4798496d38cc680888cbd8affe77fb9345
Bhanu <k_redu@outlook.com>
2024-01-02 20:58:10 +0530

    new changes

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Hello world, Iam Bhanu learning AWS Devops" 
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: Iam fine Thank you $PERSON1"
+echo "$PERSON1: Thank you git branch $PERSON2"
