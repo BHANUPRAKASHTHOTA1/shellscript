d303b7080ec6b20150b1ca214a446cde2669defb
Bhanu <k_redu@outlook.com>
2024-01-02 20:19:41 +0530

    new changes

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -4,8 +4,8 @@ echo "Hello world, Iam Bhanu learning AWS Devops"
 PERSON1=bhanu
 PERSON2=prakash
 
-echo "PERSON1: Hi $PERSON2"
-echo "PERSON2: Hello $PERSON1"
-echo "PERSON1: How are you $PERSON2"
-echo "PERSON2: Iam fine Thank you $PERSON1"
-echo "PERSON1: Thank you git branch -$PERSON2"
+echo "$PERSON1: Hi $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: Iam fine Thank you $PERSON1"
+echo "$PERSON1: Thank you git branch -$PERSON2"
