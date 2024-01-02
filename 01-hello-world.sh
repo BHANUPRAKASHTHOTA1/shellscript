f6da2ff5319e29a5aa49076ade0526afad3c8ad3
Bhanu <k_redu@outlook.com>
2024-01-02 20:08:37 +0530

    commit code

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-echo "Hello world, Iam Bhanu learning AWS Devops"
+echo "Hello world, Iam Bhanu learning AWS Devops" 
+
+PERSON1=bhanu
+PERSON2=prakash
+
+echo "PERSON1: Hi $PERSON2"
+echo "PERSON2: Hello $PERSON1"
+echo "PERSON1: How are you $PERSON2"
+echo "PERSON2: Iam fine Thank you $PERSON1"
