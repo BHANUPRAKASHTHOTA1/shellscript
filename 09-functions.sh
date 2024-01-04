151ab012e62b4b9780df3131b056c2912a69153e
Bhanu <k_redu@outlook.com>
2024-01-04 11:41:57 +0530

    new changes

---
+++ 09-functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "ERROR::Please run this script with root access"
+    exit 1
+fi
+yum install mysql -y
+
+if [ $? -ne 0 ]
+then
+    echo "Installatioin of my sql is error"
+    exit 1
+else
+    echo "Installatioin of my sql is success"
+fi
+
+yum install postfix -y
+if [ $? -ne 0 ]
+then
+    echo "Installatioin of my sql is error"
+    exit 1
+else
+    echo "Installatioin of my sql is success"
+fi
