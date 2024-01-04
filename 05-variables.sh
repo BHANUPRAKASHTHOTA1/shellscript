1c3532d89b57088ea1b710d2c90af8e1d98bf4b6
Bhanu <k_redu@outlook.com>
2024-01-04 10:42:27 +0530

    new changes

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
 echo "Please enter your username "
 read USERNAME
+echo "Entered username is: $USERNAME"
+
 echo "Please enter your password"
+read PASSWORD
+echo "Entered password is: $PASSWORD"
