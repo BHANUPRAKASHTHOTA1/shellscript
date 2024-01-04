9dc4b2cd9a3771e840731feb9089576fba8f0970
Bhanu <k_redu@outlook.com>
2024-01-04 11:49:28 +0530

    new changes

--- 09-functions.sh
+++ 09-functions.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
 USERID=$(id -u)
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo "$2.... Failure"
+        exit 1
+    else
+        echo "$2.... Success"
+    fi
+}
+
+
+
 if [ $USERID -ne 0 ]
 then
     echo "ERROR::Please run this script with root access"
     exit 1
 fi
 yum install mysql -y
-
-if [ $? -ne 0 ]
-then
-    echo "Installatioin of my sql is error"
-    exit 1
-else
-    echo "Installatioin of my sql is success"
-fi
+VALIDATE $? "Installing MYSQL"
 
 yum install postfix -y
-if [ $? -ne 0 ]
-then
-    echo "Installatioin of Postfix is error"
-    exit 1
-else
-    echo "Installatioin of Postfix is success"
-fi
+VALIDATE $? "Installing postfix"
