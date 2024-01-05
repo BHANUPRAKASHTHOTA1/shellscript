c3b483025199d0d831ebb3c5e98b3958b192fb86
Bhanu <k_redu@outlook.com>
2024-01-05 12:17:38 +0530

    Added new code

--- 12-loops.sh
+++ 12-loops.sh
@@ -22,3 +22,27 @@ then
     echo -e "$R ERROR::Please run the script with root access $N"
     exit 1
 fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}
+for i in $@
+do
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed ,let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
+    
+
+done
